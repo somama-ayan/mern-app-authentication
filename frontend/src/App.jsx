fab474c83587c0bd490962a855301ad6a7a829f4
Somama Ayan <somama.ayaan@gmail.com>
2025-03-09 01:53:25 +0500

    app and tailwind insatalled

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-bold text-4xl text-red-500'>hello world</h1>
+      <p className='text-4xl text-red-500'>hello world</p>
+    </>
+  )
+}
+
+export default App
